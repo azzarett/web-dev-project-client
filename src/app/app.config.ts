f590ae1271084547bbc8347e80846e9e3312f85f
azzarett <135713210+azzarett@users.noreply.github.com>
2025-04-22 09:12:00 +0500

    feat: Implement authentication components and routing (#1)
    - Refactored app.component to use standalone component with router outlet.
    - Updated app.config to include necessary providers for HTTP client, animations, and forms.
    - Added routes for login and registration components.
    - Created LoginComponent with reactive form for user login, including HTML, SCSS, and spec files.
    - Created RegisterComponent with reactive form for user registration, including HTML, SCSS, and spec files.
    - Added custom theme support for NG-ZORRO.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(),
+  ],
 };
